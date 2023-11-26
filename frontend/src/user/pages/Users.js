f4a607dcb2eb6ae42078a1f2d3bb3d3ebbf9fe9b
Harun Hadzagic <107354156+harun-hadzagic@users.noreply.github.com>
2023-11-26 20:55:10 +0100

    Adding header and user info display

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,7 +1,20 @@
 import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-  return <div>Users works</div>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "Hara",
+      image: "https://th.bing.com/th/id/OIG.ey_KYrwhZnirAkSgDhmg",
+      places: 3,
+    },
+  ];
+  return (
+    <div>
+      <UsersList items={USERS} />
+    </div>
+  );
 };
 
 export default Users;
